ae12918b8939aa80180c792133b202f712ccea7d
Uotila Terho T <ttuotila@cc.helsinki.fi>
2016-05-27 16:13:43 +0300

    sivuja

--- app/controllers/hello_world_controller.php
+++ app/controllers/hello_world_controller.php
@@ -1,14 +1,50 @@
 <?php
 
-  class HelloWorldController extends BaseController{
+class HelloWorldController extends BaseController {
 
-    public static function index(){
-      // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
-   	  View::make('home.html');
+    public static function index() {
+        // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
+        View::make('home.html');
     }
 
-    public static function sandbox(){
-      // Testaa koodiasi täällä
-      echo 'Hello World!';
+    public static function sandbox() {
+        // Testaa koodiasi täällä
+        View::make('helloworld.html');
     }
-  }
+
+    public static function etusivu() {
+        // Testaa koodiasi täällä
+        View::make('etusivu.html');
+    }
+
+    public static function kirjaudu() {
+        // Testaa koodiasi täällä
+        View::make('kirjaudu_rekisteroidy.html');
+    }
+
+    public static function omat_tiedot() {
+        // Testaa koodiasi täällä
+        View::make('omat_tiedot.html');
+    }
+
+    public static function ajopaivakirja() {
+        // Testaa koodiasi täällä
+        View::make('ajopaivakirja.html');
+    }
+
+    public static function lisaa_ajopaivakirja() {
+        // Testaa koodiasi täällä
+        View::make('uusi_ajopaivakirja.html');
+    }
+
+    public static function lisaa_tera() {
+        // Testaa koodiasi täällä
+        View::make('lisaa_tera.html');
+    }
+
+    public static function lisaa_hoyla() {
+        // Testaa koodiasi täällä
+        View::make('lisaa_hoyla.html');
+    }
+
+}
--- config/routes.php
+++ config/routes.php
@@ -1,9 +1,37 @@
 <?php
 
-  $routes->get('/', function() {
+$routes->get('/', function() {
     HelloWorldController::index();
-  });
+});
 
-  $routes->get('/hiekkalaatikko', function() {
+$routes->get('/hiekkalaatikko', function() {
     HelloWorldController::sandbox();
-  });
+});
+
+$routes->get('/etusivu', function() {
+    HelloWorldController::etusivu();
+});
+
+$routes->get('/kirjaudu', function() {
+    HelloWorldController::kirjaudu();
+});
+
+$routes->get('/omat_tiedot', function() {
+    HelloWorldController::omat_tiedot();
+});
+
+$routes->get('/uusi_tera', function() {
+    HelloWorldController::lisaa_tera();
+});
+
+$routes->get('/uusi_hoyla', function() {
+    HelloWorldController::lisaa_hoyla();
+});
+
+$routes->get('/ajopaivakirja', function() {
+    HelloWorldController::ajopaivakirja();
+});
+
+$routes->get('/uusi_ajopaivakirja', function() {
+    HelloWorldController::lisaa_ajopaivakirja();
+});
