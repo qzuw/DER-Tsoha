2618c1f0627a32671ccc68156b524ff468fc77b8
Uotila Terho T <ttuotila@cc.helsinki.fi>
2016-05-30 18:52:34 +0300

    sql-taulut

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,45 @@
 -- Lisää CREATE TABLE lauseet tähän tiedostoon
+CREATE TABLE Kayttaja(
+  id SERIAL PRIMARY KEY, 
+  tunnus varchar(50) NOT NULL, 
+  salasana varchar(50) NOT NULL,
+  yllapito boolean
+);
+
+CREATE TABLE Partahoyla(
+  id SERIAL PRIMARY KEY, 
+  valmistaja varchar(50) NOT NULL, 
+  malli varchar(50) NOT NULL,
+  poistettu BOOLEAN,
+  aggressiivisuus REAL,
+  viittauksia INTEGER
+);
+
+CREATE TABLE KayttajanHoylat(
+  id SERIAL PRIMARY KEY, 
+  kayttaja_id INTEGER REFERENCES Kayttaja(id),
+  partahoyla_id INTEGER REFERENCES Partahoyla(id),
+);
+
+CREATE TABLE Tera(
+  id SERIAL PRIMARY KEY, 
+  valmistaja varchar(50) NOT NULL, 
+  malli varchar(50) NOT NULL,
+  poistettu BOOLEAN,
+  teravyys REAL,
+  pehmeys REAL,
+  viittauksia INTEGER
+);
+
+CREATE TABLE Paivakirja(
+  id SERIAL PRIMARY KEY, 
+  kayttaja_id INTEGER REFERENCES Kayttaja(id),
+  partahoyla_id INTEGER REFERENCES Partahoyla(id),
+  tera_id INTEGER REFERENCES Tera(id),
+  pvm TIMESTAMP NOT NULL,
+  saippua varchar(50) NOT NULL, 
+  yleiskommentit text NOT NULL,
+  kommentit_terasta text NOT NULL,
+  kommentit_hoylasta text NOT NULL
+);
+
--- sql/drop_tables.sql
+++ sql/drop_tables.sql
@@ -1,1 +1,6 @@
 -- Lisää DROP TABLE lauseet tähän tiedostoon
+Drop Table IF EXISTS Kayttaja CASCADE;
+Drop Table IF EXISTS KayttajanHoylat CASCADE;
+Drop Table IF EXISTS Partahoyla CASCADE;
+Drop Table IF EXISTS Paivakirja CASCADE;
+Drop Table IF EXISTS Tera CASCADE;
