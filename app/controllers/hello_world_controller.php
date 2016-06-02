2717587841e556cb7093edc622dbd6fbfb48f68c
Uotila Terho T <ttuotila@cc.helsinki.fi>
2016-06-02 21:15:00 +0300

    laitetaan nyt suunnitelmat omaan kansioon ohjeen mukaan

--- app/controllers/hello_world_controller.php
+++ app/controllers/hello_world_controller.php
@@ -14,52 +14,52 @@ class HelloWorldController extends BaseController {
 
     public static function etusivu() {
         // Testaa koodiasi täällä
-        View::make('etusivu.html');
+        View::make('suunnitelmat/etusivu.html');
     }
 
     public static function kirjaudu() {
         // Testaa koodiasi täällä
-        View::make('kirjaudu_rekisteroidy.html');
+        View::make('suunnitelmat/kirjaudu_rekisteroidy.html');
     }
 
     public static function omat_tiedot() {
         // Testaa koodiasi täällä
-        View::make('omat_tiedot.html');
+        View::make('suunnitelmat/omat_tiedot.html');
     }
 
     public static function ajopaivakirja() {
         // Testaa koodiasi täällä
-        View::make('nayta_pvk.html');
+        View::make('suunnitelmat/nayta_pvk.html');
     }
 
     public static function listaa_ajot() {
         // Testaa koodiasi täällä
-        View::make('listaa_pvkirja.html');
+        View::make('suunnitelmat/listaa_pvkirja.html');
     }
 
     public static function lisaa_ajopaivakirja() {
         // Testaa koodiasi täällä
-        View::make('uusi_ajopaivakirja.html');
+        View::make('suunnitelmat/uusi_ajopaivakirja.html');
     }
 
     public static function lisaa_tera() {
         // Testaa koodiasi täällä
-        View::make('lisaa_tera.html');
+        View::make('suunnitelmat/lisaa_tera.html');
     }
 
     public static function lisaa_hoyla() {
         // Testaa koodiasi täällä
-        View::make('lisaa_hoyla.html');
+        View::make('suunnitelmat/lisaa_hoyla.html');
     }
 
     public static function nayta_tera() {
         // Testaa koodiasi täällä
-        View::make('nayta_tera.html');
+        View::make('suunnitelmat/nayta_tera.html');
     }
 
     public static function nayta_hoyla() {
         // Testaa koodiasi täällä
-        View::make('nayta_hoyla.html');
+        View::make('suunnitelmat/nayta_hoyla.html');
     }
 
 }
