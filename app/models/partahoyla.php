ec79e54c6abeffaaa14283dfaa27b7a9d342782f
Uotila Terho T <ttuotila@cc.helsinki.fi>
2016-06-02 21:28:08 +0300

    partahoyla

--- app/models/partahoyla.php
+++ app/models/partahoyla.php
@@ -9,11 +9,38 @@ class Partahoyla extends BaseModel {
     }
 
     public static function all(){
-        
+        $query = DB::connection()->prepare('SELECT * FROM Partahoyla');
+	$query->execute();
+	$rows = $query->fetchAll();
+	$hoylat = array();
+	foreach($rows as $row){
+		$hoylat[] = new Partahoyla(array(
+			'id' => $row['id'],
+			'valmistaja' => $row['valmistaja'],
+			'malli' => $row['malli'],
+			'aggressiivisuus' => $row['aggressiivisuus'],
+			'viittauksia' => $row['viittauksia']
+		));
+	}
+	return $hoylat;
     }
     
     public static function find($id){
-        
+        $query = DB::connection()->prepare('SELECT * FROM Partahoyla WHERE id = :id');
+	$query->execute(array('id' => $id));
+	$row = $query->fetch();
+
+	if($row){
+		$hoyla = new Partahoyla(array(
+			'id' => $row['id'],
+			'valmistaja' => $row['valmistaja'],
+			'malli' => $row['malli'],
+			'aggressiivisuus' => $row['aggressiivisuus'],
+			'viittauksia' => $row['viittauksia']
+		));
+		return $hoyla;
+	}
+	return null;
     }
     
 }
