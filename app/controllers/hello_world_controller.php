299be6031a13c92927a8f25400db42011dec634d
Uotila Terho T <ttuotila@cc.helsinki.fi>
2016-06-04 23:56:41 +0300

    teralistaussuunnitelma

--- app/controllers/hello_world_controller.php
+++ app/controllers/hello_world_controller.php
@@ -25,57 +25,50 @@ class HelloWorldController extends BaseController {
     }
 
     public static function helloworld() {
-        // Testaa koodiasi täällä
         View::make('helloworld.html');
     }
 
     public static function etusivu() {
-        // Testaa koodiasi täällä
         View::make('suunnitelmat/etusivu.html');
     }
 
     public static function kirjaudu() {
-        // Testaa koodiasi täällä
         View::make('suunnitelmat/kirjaudu_rekisteroidy.html');
     }
 
     public static function omat_tiedot() {
-        // Testaa koodiasi täällä
         View::make('suunnitelmat/omat_tiedot.html');
     }
 
     public static function ajopaivakirja() {
-        // Testaa koodiasi täällä
         View::make('suunnitelmat/nayta_pvk.html');
     }
 
     public static function listaa_ajot() {
-        // Testaa koodiasi täällä
         View::make('suunnitelmat/listaa_pvkirja.html');
     }
 
     public static function lisaa_ajopaivakirja() {
-        // Testaa koodiasi täällä
         View::make('suunnitelmat/uusi_ajopaivakirja.html');
     }
 
     public static function lisaa_tera() {
-        // Testaa koodiasi täällä
         View::make('suunnitelmat/lisaa_tera.html');
     }
 
     public static function lisaa_hoyla() {
-        // Testaa koodiasi täällä
         View::make('suunnitelmat/lisaa_hoyla.html');
     }
 
     public static function nayta_tera() {
-        // Testaa koodiasi täällä
         View::make('suunnitelmat/nayta_tera.html');
     }
 
+    public static function listaa_terat() {
+        View::make('suunnitelmat/listaa_terat.html');
+    }
+
     public static function nayta_hoyla() {
-        // Testaa koodiasi täällä
         View::make('suunnitelmat/nayta_hoyla.html');
     }
 
