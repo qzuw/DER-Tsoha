f92189e7d86ac4d68386b8214d13874677a33288
Uotila Terho T <ttuotila@cc.helsinki.fi>
2016-06-04 23:31:14 +0300

    testidataa

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,9 +1,26 @@
 -- Lisää INSERT INTO lauseet tähän tiedostoon
 INSERT INTO Kayttaja (id, tunnus, salasana) VALUES (1, 'Parranajaja', 'salasana123');
-INSERT INTO Kayttaja (id, tunnus, salasana) VALUES (2, 'Admin', 'salasana321');
+INSERT INTO Kayttaja (id, tunnus, salasana) VALUES (2, 'Partapekka', 'salasana321');
 
 INSERT INTO Tera (valmistaja, malli) VALUES ('Astra', 'Superior Platinum');
-INSERT INTO Tera (valmistaja, malli) VALUES ('Personna', 'Blue');
+INSERT INTO Tera (valmistaja, malli) VALUES ('Personna', 'Platinum');
+INSERT INTO Tera (valmistaja, malli) VALUES ('Rapira', 'Platinum');
+INSERT INTO Tera (valmistaja, malli) VALUES ('Rapira', 'Voskhod');
+INSERT INTO Tera (valmistaja, malli) VALUES ('Polsilver', 'Super Iridium');
+INSERT INTO Tera (valmistaja, malli) VALUES ('Rapira', 'Swedish Supersteel');
+INSERT INTO Tera (valmistaja, malli) VALUES ('Feather', 'Hi-Stainless');
+INSERT INTO Tera (valmistaja, malli) VALUES ('Gillette', '7 o'clock SharpEdge');
+INSERT INTO Tera (valmistaja, malli) VALUES ('Gillette', '7 o'clock Super Stainless');
+INSERT INTO Tera (valmistaja, malli) VALUES ('Bolzano', 'superinox');
+INSERT INTO Tera (valmistaja, malli) VALUES ('Astra', 'Superior Stainless');
+INSERT INTO Tera (valmistaja, malli) VALUES ('Shark', 'Super Stainless');
+INSERT INTO Tera (valmistaja, malli) VALUES ('Derby', 'Extra');
+INSERT INTO Tera (valmistaja, malli) VALUES ('Treet', 'Platinum');
+INSERT INTO Tera (valmistaja, malli) VALUES ('Treet', 'Classic');
+INSERT INTO Tera (valmistaja, malli) VALUES ('Treet', 'Dura Sharp');
+INSERT INTO Tera (valmistaja, malli) VALUES ('Lord', 'Big Ben');
+INSERT INTO Tera (valmistaja, malli) VALUES ('Perma-Sharp', 'Super');
+INSERT INTO Tera (valmistaja, malli) VALUES ('Tiger', 'Superior Stainless');
 
 INSERT INTO Partahoyla (id, valmistaja, malli) VALUES (1, 'Parker', '24C');
 INSERT INTO Partahoyla (id, valmistaja, malli) VALUES (2, 'Merkur', '34c');
