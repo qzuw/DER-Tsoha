917f5636a65670f1fcc88f09c2adb942c6bfe690
Uotila Terho T <ttuotila@cc.helsinki.fi>
2016-06-06 12:57:40 +0300

    formatointi kuntoon netbeansin avulla

--- app/controllers/terat_controller.php
+++ app/controllers/terat_controller.php
@@ -1,31 +1,33 @@
 <?php
 
-class TeraController extends BaseController{
-  public static function index(){
-    $terat = Tera::all();
-    View::make('listaa_terat.html', array('terat' => $terat));
-  }
+class TeraController extends BaseController {
 
-  public static function nayta($id){
-    $tera = Tera::find($id);
-    View::make('nayta_tera.html', array('tera' => $tera));
-  }
-
-  public static function uusi(){
-    View::make('lisaa_tera.html');
-  }
-
-  public static function lisaa(){
-    $params = $_POST;
-    $tera = new Tera(array(
-      'valmistaja' => $params['valmistaja'],
-      'malli' => $params['malli']
-    ));
-    $onnistui = $tera->add();
-    if ($onnistui) {
-      Redirect::to('/nayta_tera/' . $tera->id, array('message' => 'Terä on nyt lisätty tietokantaan'));
-    } else {
-      Redirect::to('/listaa_terat', array('error' => 'Terän lisääminen tietokantaan epäonnistui, tarkista onko se listassa jo ennestään'));
+    public static function index() {
+        $terat = Tera::all();
+        View::make('listaa_terat.html', array('terat' => $terat));
     }
-  }
+
+    public static function nayta($id) {
+        $tera = Tera::find($id);
+        View::make('nayta_tera.html', array('tera' => $tera));
+    }
+
+    public static function uusi() {
+        View::make('lisaa_tera.html');
+    }
+
+    public static function lisaa() {
+        $params = $_POST;
+        $tera = new Tera(array(
+            'valmistaja' => $params['valmistaja'],
+            'malli' => $params['malli']
+        ));
+        $onnistui = $tera->add();
+        if ($onnistui) {
+            Redirect::to('/nayta_tera/' . $tera->id, array('message' => 'Terä on nyt lisätty tietokantaan'));
+        } else {
+            Redirect::to('/listaa_terat', array('error' => 'Terän lisääminen tietokantaan epäonnistui, tarkista onko se listassa jo ennestään'));
+        }
+    }
+
 }
--- app/models/partahoyla.php
+++ app/models/partahoyla.php
@@ -8,39 +8,39 @@ class Partahoyla extends BaseModel {
         parent::__construct($attributes);
     }
 
-    public static function all(){
+    public static function all() {
         $query = DB::connection()->prepare('SELECT * FROM Partahoyla');
-	$query->execute();
-	$rows = $query->fetchAll();
-	$hoylat = array();
-	foreach($rows as $row){
-		$hoylat[] = new Partahoyla(array(
-			'id' => $row['id'],
-			'valmistaja' => $row['valmistaja'],
-			'malli' => $row['malli'],
-			'aggressiivisuus' => $row['aggressiivisuus'],
-			'viittauksia' => $row['viittauksia']
-		));
-	}
-	return $hoylat;
+        $query->execute();
+        $rows = $query->fetchAll();
+        $hoylat = array();
+        foreach ($rows as $row) {
+            $hoylat[] = new Partahoyla(array(
+                'id' => $row['id'],
+                'valmistaja' => $row['valmistaja'],
+                'malli' => $row['malli'],
+                'aggressiivisuus' => $row['aggressiivisuus'],
+                'viittauksia' => $row['viittauksia']
+            ));
+        }
+        return $hoylat;
     }
-    
-    public static function find($id){
-        $query = DB::connection()->prepare('SELECT * FROM Partahoyla WHERE id = :id');
-	$query->execute(array('id' => $id));
-	$row = $query->fetch();
 
-	if($row){
-		$hoyla = new Partahoyla(array(
-			'id' => $row['id'],
-			'valmistaja' => $row['valmistaja'],
-			'malli' => $row['malli'],
-			'aggressiivisuus' => $row['aggressiivisuus'],
-			'viittauksia' => $row['viittauksia']
-		));
-		return $hoyla;
-	}
-	return null;
+    public static function find($id) {
+        $query = DB::connection()->prepare('SELECT * FROM Partahoyla WHERE id = :id');
+        $query->execute(array('id' => $id));
+        $row = $query->fetch();
+
+        if ($row) {
+            $hoyla = new Partahoyla(array(
+                'id' => $row['id'],
+                'valmistaja' => $row['valmistaja'],
+                'malli' => $row['malli'],
+                'aggressiivisuus' => $row['aggressiivisuus'],
+                'viittauksia' => $row['viittauksia']
+            ));
+            return $hoyla;
+        }
+        return null;
     }
-    
+
 }
