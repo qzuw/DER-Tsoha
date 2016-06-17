b34d55ea617d67986b3ab45e7007357113702853
Uotila Terho T <ttuotila@cc.helsinki.fi>
2016-06-17 17:08:56 +0300

    hieman lisää testidataa

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -23,13 +23,15 @@ INSERT INTO Tera (valmistaja, malli) VALUES ('Perma-Sharp', 'Super');
 INSERT INTO Tera (valmistaja, malli) VALUES ('Tiger', 'Superior Stainless');
 
 INSERT INTO Partahoyla (id, valmistaja, malli) VALUES (1, 'Parker', '24C');
-INSERT INTO Partahoyla (valmistaja, malli) VALUES ('Merkur', '34C HD');
-INSERT INTO Partahoyla (valmistaja, malli) VALUES ('Pearl', 'SSH-02');
-INSERT INTO Partahoyla (valmistaja, malli) VALUES ('Merkur', '37C');
-INSERT INTO Partahoyla (valmistaja, malli) VALUES ('Fatip', 'Piccolo');
+INSERT INTO Partahoyla (id, valmistaja, malli) VALUES (2, 'Merkur', '34C HD');
+INSERT INTO Partahoyla (id, valmistaja, malli) VALUES (3, 'Pearl', 'SSH-02');
+INSERT INTO Partahoyla (id, valmistaja, malli) VALUES (4, 'Fatip', 'Piccolo');
+INSERT INTO Partahoyla (id, valmistaja, malli) VALUES (5, 'Merkur', '37C');
 
 INSERT INTO KayttajanHoylat (partahoyla_id, kayttaja_id) VALUES (1, 1);
 INSERT INTO KayttajanHoylat (partahoyla_id, kayttaja_id) VALUES (2, 1);
 INSERT INTO KayttajanHoylat (partahoyla_id, kayttaja_id) VALUES (1, 2);
 
-INSERT INTO Paivakirja (partahoyla_id, kayttaja_id, tera_id, pvm, saippua, kommentit, julkisuus) VALUES (1, 1, 1, '2016-04-14 00:00:00', 'MWF', 'Hyvä ajo, vakiokombo Parker, Astra ja MWF ei taaskaan pettänyt. Ajon viimeisteli Nivean aftershave-balsami.', true);
+INSERT INTO Paivakirja (partahoyla_id, kayttaja_id, tera_id, pvm, saippua, kommentit, julkisuus) VALUES (1, 1, 1, '2016-04-14 18:00:00', 'MWF', 'Hyvä ajo, vakiokombo Parker, Astra ja MWF ei taaskaan pettänyt. Ajon viimeisteli Nivean aftershave-balsami.', TRUE);
+INSERT INTO Paivakirja (partahoyla_id, kayttaja_id, tera_id, pvm, saippua, kommentit, julkisuus) VALUES (4, 1, 1, '2016-06-14 19:00:00', 'MWF', 'Huolimatta MWN tarjoamasta suojasta, veri virtasi vuolaasti. Paikkailin haavoja alunalla.', FALSE);
+INSERT INTO Paivakirja (partahoyla_id, kayttaja_id, tera_id, pvm, saippua, kommentit, julkisuus) VALUES (1, 2, 1, '2016-06-12 21:00:00', 'LEA Classic-voide', 'Perusajo Parkerilla ja Astralla. LEA ei ehkä ole liukkain mahdollinen, mutta mukava tuoksu. Ajon viimeisteli jälleen Nivean aftershave-balsami.', TRUE);
